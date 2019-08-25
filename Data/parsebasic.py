48b4824d472d04fb770b716815e73d3be078fc2d
benhamlin314 <bhamlin314@gmail.com>
2019-08-24 17:18:26 -0700

    update parsebasic #1

--- Data/parsebasic.py
+++ Data/parsebasic.py
@@ -3,4 +3,11 @@
 import psycopg
 
 # read title.basics.tsv
-# parse to be inputted to title_basics, title_genre tables
+# parsed data to be inputted to title_basics, title_genre tables
+# this parser must be ran first due to dependencies in the database
+
+def main():
+
+
+if __name__ == '__main__':
+    main()
