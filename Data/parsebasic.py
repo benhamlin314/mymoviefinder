1f26d19a76287b3b34c19fa57768d9eba10dc837
benhamlin314 <bhamlin314@gmail.com>
2019-08-29 16:23:31 -0700

    parsing basics successful to print #1

--- Data/parsebasic.py
+++ Data/parsebasic.py
@@ -1,13 +1,23 @@
 #!/usr/bin/python3
 
-import psycopg
+import psycopg2
+import csv
 
 # read title.basics.tsv
 # parsed data to be inputted to title_basics, title_genre tables
-# this parser must be ran first due to dependencies in the database
+# this parser must run first due to dependencies in the database
 
 def main():
-
+    with open("title.basics.tsv/data.tsv", "rt", newline='') as file:
+        # open database connection to postgres database
+        # conn = psycopg2.connect("dbname=mymoviefinder user=postgres")
+        # cur = conn.cursor()
+        reader = csv.DictReader(file, delimiter="\t")
+        for field in reader.fieldnames:
+            print(field)
+        for row in reader:
+            # input data in table
+            print(row['tconst'], row['titleType'], row['primaryTitle'], row['originalTitle'], row['isAdult'], row['startYear'], row['endYear'], row['runtimeMinutes'], row['genres'])
 
 if __name__ == '__main__':
     main()
